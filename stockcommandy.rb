36323f053656a2e4864f99c3eccbf4f0aad376df
Sai Avala <avala.sai@gmail.com>
2016-02-14 23:56:03 -0500

    Updated Release

--- stockcommandy.rb
+++ stockcommandy.rb
@@ -5,9 +5,9 @@
 class Stockcommandy < Formula
   desc "CLI tool for querying and creating a watchlist for your stocks"
   homepage "https://github.com/savala/stockCommandy"
-  url "https://github.com/savala/stockCommandy/archive/0.0.8.zip"
-  version "0.0.8"
-  sha1 "142a20a7b73e720ca9fd56d6c23d36b6d160fb50"
+  url "https://github.com/savala/stockCommandy/archive/0.0.9.zip"
+  version "0.0.9"
+  sha1 "f79bf14451bc883d9d93d7db8479e27306208bf9"
   # sha256 "32c14ea6a872fa2735bd0148fa9ba71cf993153e1ce53deea2855ffc86dac967"
 
   # depends_on "cmake" => :build
